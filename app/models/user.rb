9d26a1453e940f8395106aebd13f5f9c3d1d5840
Ubuntu <ubuntu@ip-172-31-37-58.ap-northeast-1.compute.internal>
2022-05-03 14:22:07 +0000

    show

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save { self.email.downcase! }
   
   # nilは許さず、50文字以内
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 20 }
   # nilは許さず、50文字以内、メールアドレス形式、重複は許さず
   validates :email, presence: true, length: { maximum: 255 },
    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
